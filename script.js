3ca2c4152c2fb04314db6f9263ad20bd05068a99
Glitch (mylvaganam) <none>
2024-02-26 03:30:51 +0000

    🐇🐝 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,313 @@
+let greetings = [
+  // "<i>RAGE MORE</i>", "great minds", "welcome", "hello",
+  "☆ WELCOME TO MY OYSTER ☆ WELCOME TO MY OYSTER ☆ WELCOME TO MY OYSTER ☆ WELCOME TO MY OYSTER ☆ WELCOME TO MY OYSTER ☆",
+];
+
+let about = [
+  {
+    image:
+      "<img src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/meid.jpg?v=1707518188987' alt='me' width='105.4' height='141.6'/><br><br>",
+    blurb:
+      "Mylvaganam is a filmmaker and coder interested in slow cinema, queer intimacies, and experimental media. She holds a B.A. in Radio/Television/Film and Computer Science from Northwestern University and currently works in the production studio at the National Gallery of Art in Washington, DC. She likes herself best when she’s passionate.",
+    contact:
+      "<br><br><a href='mailto:mylvaganamamelia@gmail.com'>email</a> <a target=_blank href='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/2024%20Mylvaganam%20resume.pdf?v=1708829819188'>cv</a> <a href='https://instagram.com/myylvaganam' target='_blank'>instagram</a>",
+  },
+];
+
+let projects = [
+  {
+    name: "Dreamlogic",
+    username: "dreamlogic",
+    centeralign:
+      "A glimpse into the collision of M and J, two young people who find desire in each other. Slow intimacy, queer yearning, power, and tenderness.",
+    leftalign:
+      "director/editor <b>Mylvaganam</b><br>producers <b>Eli Han</b> and <b>İsmet Mert Küçüker</b><br>director of photography <b>Haoshu Sascha Deng</b><br>composition <b>ballet moshers</b><br>starring <b>Jacky Herrera</b> and <b>Maira Walker</b><br><br>16mm color<br>2023<br>14mins",
+    centeragain: "",
+    images: [
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/1dreamlogic25.jpg?v=1707639087122' alt='still from Dreamlogic'>",
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/2dreamlogic1.jpg?v=1707639087504' alt='still from Dreamlogic'>",
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/haircut.jpg?v=1708666611718' alt='still from Dreamlogic'>",
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/4dreamlogic10.jpg?v=1707639088310' alt='still from Dreamlogic'>",
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/5dreamlogic19.jpg?v=1707639088722' alt='still from Dreamlogic'>",
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/6dreamlogic24.jpg?v=1707639089000' alt='still from Dreamlogic'>",
+    ],
+    embedcode: "",
+  },
+  {
+    name: "In Remembrance of a Bottle",
+    username: "inremembrance",
+    centeralign:
+      "Flicker* study of shattering glass, pooling liquid, and simultaneous intimacies.<br>(cut from footage shot for <a href='#dreamlogic'><i>Dreamlogic</i></a>)<br>*photosensitive viewers proceed with caution",
+    leftalign:
+      "director/editor <b>Mylvaganam</b><br>production <b>Dreamlogic team</b><br><br>16mm color<br>2023<br>87sec",
+    centeragain: "",
+    images: [
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/inremembrance1.jpg?v=1707635511975' alt='still from In Remembrance of a Bottle'>",
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/inremembrance2.jpg?v=1707635512333' alt='still from In Remembrance of a Bottle'>",
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/inremembrance5.jpg?v=1707635520563' alt='still from In Remembrance of a Bottle'>",
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/inremembrance4.jpg?v=1707635520113' alt='still from In Remembrance of a Bottle'>",
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/inremembrance6.jpg?v=1707635520994' alt='still from In Remembrance of a Bottle'>",
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/inremembrance7.jpg?v=1707635521476' alt='still from In Remembrance of a Bottle'>",
+    ],
+    embedcode: "",
+  },
+  {
+    name: "Humdrum Ecstasy",
+    username: "humdrum",
+    centeralign:
+      "A woman caught in a monotonous life finds moments where reality and dreams blur.",
+    leftalign:
+      "writer/director <b>Haoshu Sascha Deng</b><br>producers <b>Andrew Kline</b> and <b>Sophia Pedro Torres</b><br>cinematography <b>Mylvaganam</b><br>starring <b>JG Singhal</b> and <b>Eli Han</b><br><br>2K B&W<br>filmed August 2023, currently in post-production",
+    centeragain: "",
+    images: [
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/humdrum1?v=1707525894876' alt='still from Humdrum Ecstasy'>",
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/humdrum2?v=1707525906323' alt='still from Humdrum Ecstasy'>",
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/humdrum4?v=1707525905931' alt='still from Humdrum Ecstasy'>",
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/humdrum3?v=1707525901178' alt='still from Humdrum Ecstasy'>",
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/humdrum6?v=1707545243960' alt='still from Humdrum Ecstasy'>",
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/humdrum5?v=1707525905574' alt='still from Humdrum Ecstasy'>",
+    ],
+    embedcode: "",
+  },
+  {
+    name: "DREAMHOTLINE",
+    username: "dreamhotline",
+    centeralign:
+      "In a near-future world, Tara works at a call center that replicates the voices of lost loved ones. An unexpected call disrupts her daily rhythm.",
+    leftalign:
+      "writer/director <b>Tanisha Tekriwal</b><br>producers <b>Shay Park</b> and <b>Stephanie Xu</b><br>cinematography <b>Mylvaganam</b><br>starring <b>Mantra Radhakrishnan</b> and <b>Manish Shah</b><br><br>4K color<br>filmed February 2023, currently in post-production",
+    centeragain: "",
+    images: [
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/1dreamhotline35.jpg?v=1708671593402' alt='still from Humdrum Ecstasy'>",
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/dreamhotline39.jpg?v=1708673145845' alt='still from Humdrum Ecstasy'>",
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/3dreamhotline2.jpg?v=1708671594550' alt='still from Humdrum Ecstasy'>",
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/4dreamhotline17.jpg?v=1708671595039' alt='still from Humdrum Ecstasy'>",
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/5dreamhotline43.jpg?v=1708671595425' alt='still from Humdrum Ecstasy'>",
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/6dreamhotline37.jpg?v=1708671595759' alt='still from Humdrum Ecstasy'>",
+    ],
+    embedcode: "",
+  },
+  {
+    name: "Shelf Life",
+    username: "shelflife",
+    centeralign:
+      "A woman finds a mysterious video discussing the female eating habits of a dystopian society.",
+    leftalign:
+      "writer/director <b>Erin Zhang</b><br>producers <b>Tanisha Tekriwal</b> and <b>Sofia Veraza</b><br>cinematography <b>Mylvaganam</b> and <b>Haoshu Sascha Deng</b><br><br>4K color<br>2022",
+    centeragain: "Atlanta Film Festival 2023<br><a href ='https://dailynorthwestern.com/2022/05/24/ae/shelf-life-goes-small-scale-for-the-big-screen-student-produced-film-uses-stop-motion-to-fit-its-set-on-pool-table/' target='_blank'>Daily Northwestern article</a>",
+    images: [],
+    embedcode:
+      "<video controls controlslist=nodownload><source src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/woman%203%20take%202%20(1).mp4?v=1708705578182' type='video/mp4' /></video>",
+  },
+  
+  {
+    name: "Daffodils",
+    username: "daffodils",
+    centeralign:
+      "An triptych portrait of the filmmaker's most intimate relationships.",
+    leftalign:
+      "made by <b>Mylvaganam</b><br>featuring <b>Eli Han</b> and <i>Web</i> by 070 Shake<br><br>HD color<br>2023",
+    centeragain: "",
+    images: [],
+    embedcode:
+      "<iframe title='vimeo-player' src='https://player.vimeo.com/video/830485654?h=420ffbf02e' width='640' height='360' frameborder='0'    allowfullscreen></iframe>",
+  },
+  {
+    name: "skateaway",
+    username: "skateaway",
+    centeralign:
+      "Two friends skateboard to a party while telling each other stories a la Eugene Lim.",
+    leftalign:
+      "recording/mixing <b>Mylvaganam</b><br>featuring <b>Eli Han</b> and <b>Mylvaganam</b><br><br>stereo mix<br>2022",
+    centeragain: "",
+    images: [],
+    embedcode:
+      "<iframe width='560' height='315' src='https://www.youtube.com/embed/Sht0H3Idgf8?si=zPMyZKGj78XINQSx' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>",
+  },
+  {
+    name: "Dear journal,",
+    username: "journal",
+    centeralign:
+      "<a href='https://dearjournal.glitch.me' target=_blank>click here for the site</a>",
+    leftalign:
+      "<i>Dear journal,</i> is a continuous collection of people sharing their journal entries of varying forms. On each load, you are presented with a random clip and its associated color. I made it in hopes of providing moments of connection for people in a very isolating time. Hearing someone's voice is an intimate thing. There is also a catalog page, if you wish to be the master of your own navigation. It's a live project that continues to grow. Submit a clip to be added <a href='https://forms.gle/9yHF4ZmSEyF32ji56' target=_blank>here</a>.<br><br>webpage<br>2021",
+    centeragain: "",
+    images: ["<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/journal1.png?v=1708915471810' alt='screenshot from dear journal, site'>",
+            "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/journal2.png?v=1708915472201' alt='screenshot from dear journal, site'>",
+            "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/journal3.png?v=1708914591831' alt='screenshot from dear journal, site'>",
+            "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/journal4.png?v=1708914592181' alt='screenshot from dear journal, site'>"],
+    embedcode:
+      "",
+  },
+  {
+    name: "sunday sites",
+    username: "sundaysites",
+    centeralign:
+      "<a href='https://commmodities.glitch.me' target=_blank>click here for my user pg</a>",
+    leftalign:
+      "<a href='https://sundaysites.cafe' target=_blank>Sunday sites</a> is monthly online gathering for participants to spend a couple hours making a site from a given prompt- pure HTML and CSS only.",
+    centeragain: "",
+    images: ["<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/petulia3.gif?v=1708917181043' alt='gif of sun/moon switch in petuliabythekooks.glitch.me'>",
+            "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/journal2.png?v=1708915472201' alt='screenshot from dear journal, site'>",
+            "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/journal3.png?v=1708914591831' alt='screenshot from dear journal, site'>",
+            "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/journal4.png?v=1708914592181' alt='screenshot from dear journal, site'>"],
+    embedcode:
+      "",
+  },
+  
+
+  // {
+  //   name: "The Band",
+  //   link: "subpages/dreamlogic.html",
+  // },
+  // {
+  //   name: "Pain Motel",
+  //   link: "subpages/dreamlogic.html",
+  // },
+  // {
+  //   name: "Daffodils",
+  //   link: "subpages/dreamlogic.html",
+  // },
+  // {
+  //   name: "19 and a half",
+  //   link: "subpages/dreamlogic.html",
+  // },
+  // {
+  //   name: "WINDOWS",
+  //   link: "subpages/dreamlogic.html",
+  // },
+  // {
+  //   name: "toast",
+  //   link: "subpages/dreamlogic.html",
+  // },
+  // {
+  //   name: "skateaway",
+  //   link: "subpages/dreamlogic.html",
+  // },
+  // {
+  //   name: "Terminal Delirium",
+  //   link: "subpages/dreamlogic.html",
+  // },
+  // 
+  // {
+  //   name: "@foodandmemorybot",
+  //   link: "subpages/dreamlogic.html",
+  // },
+  // {
+  //   name: "your journal",
+  //   link: "subpages/dreamlogic.html",
+  // },
+];
+
+//called when "I'm feeling lucky" button is pressed.
+//navigates to a random project page that is Not the one the user is currently on
+function imFeelingLucky() {
+  //use current path name to avoid opening the proj that user is already in
+  //function will be a loop until a non-current project is chosen
+  const currentUrl = window.location.href;
+  let urlPieces = currentUrl.split("#");
+  let samePage = true;
+  let currProjUser = "";
+  let newProjUser = "";
+  let index = -1;
+  //IF length==2 (>1) of urlPieces, then we are currently on a page of sorts and we should check that whatever is generated is Not == current proj username
+  //let's loop until we get a good one
+  while (samePage) {
+    if (urlPieces.length > 1) {
+      currProjUser = urlPieces[1];
+    }
+    index = Math.floor(Math.random() * projects.length);
+    newProjUser = projects[index].username;
+    if (!(currProjUser === newProjUser)) {
+      samePage = false;
+    }
+  }
+  //once we have a vetted newProjUser, open sesame
+  let projectlinkname = newProjUser + "link";
+  let projectlink = document.getElementById(projectlinkname);
+  projectlink.click();
+}
+function scrollUp() {
+  window.scrollTo(0,0);
+}
+function initMain() {
+  // alert("welcome! have fun poking around here...");
+  //home greeting
+  document.getElementById("home").innerHTML =
+    greetings[Math.floor(Math.random() * greetings.length)];
+  // listening for if home-string is clicked - reloads home pg
+  // let homestring = document.getElementById("home");
+  // homestring.addEventListener("click", function () {
+  //   window.open("https://mylvaganam.glitch.me", "_self"); // if homestring is clicked, "navigate home" aka links to load main pg
+  // });
+
+  //about content
+  document.getElementById("about").innerHTML =
+    document.getElementById("about").innerHTML +
+    about[0].image +
+    about[0].blurb +
+    about[0].contact;
+
+  //project list content
+  for (let i = 0; i < projects.length; i++) {
+    let newline = "";
+    if (i == 0) {
+      newline =
+        "<section class='titles'>FILM</section>" +
+        "<section class='subtitles'>DIRECTING</section>";
+    } else if (i == 2) {
+      newline = "<br><section class='subtitles'>CINEMATOGRAPHY</section>";
+    } else if (i == 7) {
+      newline = "<br><section class='subtitles'>PERSONAL</section>";
+    } else if (i == 11) {
+      newline = "<br><section class='subtitles'>SOUND</section>";
+    } else if (i == 12) {
+      newline = "<br><section class='titles'>WRITING</section>";
+    } else if (i == 13) {
+      newline = "<br><section class='titles'>CODE </section>";
+    }
+
+    newline =
+      newline +
+      "<a href=#" +
+      projects[i].username +
+      " onclick='scrollUp();' id = '" +
+      projects[i].username +
+      "link'>" +
+      projects[i].name +
+      "</a><br>";
+    document.getElementById("projects").innerHTML =
+      document.getElementById("projects").innerHTML + newline;
+  }
+
+  //initiating individual project content
+  for (let i = 0; i < projects.length; i++) {
+    let currproj = projects[i];
+    let newhtml = "<section class='projheading'>" + currproj.name;
+    +"</section>";
+    newhtml =
+      newhtml +
+      "<br><br><section class=projbody>" +
+      currproj.centeralign +
+      "</section>";
+    newhtml =
+      newhtml +
+      "<br><section class=projbody style='text-align:justify;'>" +
+      currproj.leftalign +
+      "</section>";
+    newhtml =
+      newhtml +
+      "<br><section class=projbody>" +
+      currproj.centeragain +
+      "</section>";
+    newhtml = newhtml + "<br><section class=projbody>";
+    for (let j = 0; j < currproj.images.length; j++) {
+      newhtml = newhtml + currproj.images[j];
+    }
+    newhtml =
+      newhtml + "<section class=embedded>" + currproj.embedcode + "</section>";
+    newhtml = newhtml + "</section>";
+    document.getElementById(currproj.username).innerHTML = newhtml;
+  }
+  window.scrollTo(0, -10);
+}
