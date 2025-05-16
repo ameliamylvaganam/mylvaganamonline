66da39ce258c8a6e21a79ccbbee05856b4601b0d
Glitch (mylvaganam) <none>
2025-05-16 23:24:58 +0000

    🚚🥀 Updated with Glitch updated bio

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let about = [
     image:
       "<img src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/meid.jpg?v=1707518188987' alt='me' width='105.4' height='141.6'/><br><br>",
     blurb:
-      "Mylvaganam is interested in slow cinema, queer intimacies, and experimental media. Being of mixed descent, her work has always existed in a middle ground: between analog and digital mediums, between creative and technical disciplines, between past and present temporalities.<br>She holds a B.A. in Radio/Television/Film and Computer Science from Northwestern University and currently works in the production studio at the National Gallery of Art in Washington, DC. She likes herself best when she’s passionate.",
+      "Mylvaganam is interested in slow cinema, queer intimacies, and experimental media. Like each of us, her work is many things at once: balancing analog and digital mediums, creative and technical disciplines, and past and present temporalities. She holds a B.A. in Radio/Television/Film and Computer Science from Northwestern University and currently works in the production studio at the National Gallery of Art in Washington, DC. She likes herself best when she’s passionate.",
     contact:
       "<br><a href='mailto:mylvaganamamelia@gmail.com'>mylvaganamamelia@gmail.com</a>&nbsp;&nbsp;&nbsp;&nbsp;<a target=_blank href='https://www.youtube.com/@Myylvaganam'>youtube</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='https://instagram.com/myylvaganam' target='_blank'>instagram</a>",
     footer:
