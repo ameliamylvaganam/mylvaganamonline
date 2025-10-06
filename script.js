4f4735b9339c6363dfd52b5ed07bc65c77141bef
ameliamylvaganam <41197477+ameliamylvaganam@users.noreply.github.com>
2025-10-06 14:03:33 -0400

    Update script.js adding event photography section

--- script.js
+++ script.js
@@ -326,6 +326,21 @@ let projects = [
     embedcode: "",
   },
 
+  {
+    name: "Event Photography",
+    username: "event",
+    centeralign:
+      "Below are some photos I've taken at weddings, film screenings, and other events. Contact me about photographing your next event!",
+    leftalign: [],
+    centeragain: "",
+    images: [
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/iceskating.gif?v=1718341838888' alt='gif of National Gallery of Art ice skating Instagram Reel'/>",
+      "<img loading='lazy' src='https://cdn.glitch.global/b63ee8f9-5e85-40c3-a1e3-1ca53d7532ee/fraufiber.gif?v=1718342851258' alt='gif of National Gallery of Art Frau Fiber Instagram Reel'/>",
+      
+    ],
+    embedcode: "",
+  },
+
   // {
   //   name: "19 and a half",
   //   link: "subpages/dreamlogic.html",
