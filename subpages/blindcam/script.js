f22199ab89efbcc0d08943746d9bcf744990286e
ameliamylvaganam <41197477+ameliamylvaganam@users.noreply.github.com>
2025-10-26 20:23:38 -0400

    adding rest of files for blindcam

---
+++ subpages/blindcam/script.js
@@ -0,0 +1,223 @@
+let photos = [
+  {
+    alttext: "glittery grave goods in an old town",
+    link: "https://cdn.glitch.global/e14fb8c9-e9e9-4069-bf85-9fa5104d9897/IMG_0193.JPG?v=1721489027676",
+    color: "hsl(15, 100%, 40%)",
+    vert: false,
+    placement: 0,
+  },
+  {
+    alttext: "nurturing",
+    link: "https://cdn.glitch.global/e14fb8c9-e9e9-4069-bf85-9fa5104d9897/IMG_0226.JPG?v=1721612205600",
+    color: "hsl(212.78, 54%, 60.78%)",
+    vert: true,
+    placement: 0,
+  },
+  {
+    alttext: "two parts of something greater",
+    link: "https://cdn.glitch.global/e14fb8c9-e9e9-4069-bf85-9fa5104d9897/IMG_0221.JPG?v=1721578530033",
+    color: "hsl(18, 100%, 50%)",
+    vert: false,
+    placement: 0,
+  },
+  {
+    alttext: "birdie's challengers moment",
+    link: "https://cdn.glitch.global/e14fb8c9-e9e9-4069-bf85-9fa5104d9897/IMG_0119.JPG?v=1721578750132",
+    color: "hsl(89, 40%, 50%)",
+    vert: false,
+    placement: 0,
+  },
+  {
+    alttext: "planeside sunside",
+    link: "https://cdn.glitch.global/e14fb8c9-e9e9-4069-bf85-9fa5104d9897/IMG_9865.JPG?v=1721611491768",
+    color: "hsl(33, 90%, 72%)",
+    vert: false,
+    placement: 0,
+  },
+  {
+    alttext: "october in july",
+    link: "https://cdn.glitch.global/e14fb8c9-e9e9-4069-bf85-9fa5104d9897/IMG_0250.JPG?v=1721624028689",
+    color: "hsl(30, 72%, 41%)",
+    vert: true,
+    placement: 0,
+  },
+  {
+    alttext: "carving",
+    link: "https://cdn.glitch.global/e14fb8c9-e9e9-4069-bf85-9fa5104d9897/IMG_9531.JPG?v=1721628486394",
+    color: "hsl(33.6, 70.09%, 58.04%)",
+    vert: false,
+    placement: 0,
+  },
+  {
+    alttext: "...",
+    link: "https://cdn.glitch.global/e14fb8c9-e9e9-4069-bf85-9fa5104d9897/IMG_9523.JPG?v=1724294182367",
+    color: "hsl(213, 60%, 85%)",
+    vert: false,
+    placement: 0,
+  },
+  {
+    alttext: "steady eye contact",
+    link: "https://cdn.glitch.global/e14fb8c9-e9e9-4069-bf85-9fa5104d9897/IMG_9539.JPG?v=1724294472100",
+    color: "hsl(44, 100%, 73%)",
+    vert: false,
+    placement: 0,
+  },
+  {
+    alttext: "communal bike",
+    link: "https://cdn.glitch.global/e14fb8c9-e9e9-4069-bf85-9fa5104d9897/IMG_9658.JPG?v=1724296684467",
+    color: "hsl(356, 74%, 63%)",
+    vert: true,
+    placement: 0,
+  },
+  {
+    alttext: "sweet sice",
+    link: "https://cdn.glitch.global/e14fb8c9-e9e9-4069-bf85-9fa5104d9897/IMG_9563.JPG?v=1724296964985",
+    color: "hsl(7, 22%, 23%)",
+    vert: false,
+    placement: 0,
+  },
+  {
+    alttext: "a losing game",
+    link: "https://cdn.glitch.global/e14fb8c9-e9e9-4069-bf85-9fa5104d9897/IMG_9601.JPG?v=1724297108228",
+    color: "hsl(91, 42%, 60%)",
+    vert: true,
+    placement: 0,
+  },
+  {
+    alttext: "pink hot",
+    link: "https://cdn.glitch.global/e14fb8c9-e9e9-4069-bf85-9fa5104d9897/IMG_9646.JPG?v=1724297271452",
+    color: "hsl(311, 90%, 50%)",
+    vert: true,
+    placement: 0,
+  },
+  {
+    alttext: "angels fallen",
+    link: "https://cdn.glitch.global/e14fb8c9-e9e9-4069-bf85-9fa5104d9897/IMG_9655.JPG?v=1724297285690",
+    color: "hsl(340, 60%, 55%)",
+    vert: true,
+    placement: 0,
+  },
+  {
+    alttext: "louis and co",
+    link: "https://cdn.glitch.global/e14fb8c9-e9e9-4069-bf85-9fa5104d9897/IMG_0217.JPG?v=1724297423373",
+    color: "hsl(91, 40%, 40%)",
+    vert: true,
+    placement: 0,
+  },
+  {
+    alttext: "thru a tiny window",
+    link: "https://cdn.glitch.global/e14fb8c9-e9e9-4069-bf85-9fa5104d9897/IMG_0237.JPG?v=1724297466244",
+    color: "hsl(340, 60%, 35%)",
+    vert: true,
+    placement: 0,
+  },
+  {
+    alttext: "winnipeg scamperer",
+    link: "https://cdn.glitch.global/e14fb8c9-e9e9-4069-bf85-9fa5104d9897/IMG_9893.JPG?v=1724297700597",
+    color: "hsl(20, 95%, 50%)",
+    vert: false,
+    placement: 0,
+  },
+  {
+    alttext: "palms up",
+    link: "https://cdn.glitch.global/e14fb8c9-e9e9-4069-bf85-9fa5104d9897/IMG_9676.JPG?v=1727305988387",
+    color: "hsl(20, 95%, 50%)",
+    vert: true,
+    placement: 0,
+  },
+  {
+    alttext: "mission",
+    link: "https://cdn.glitch.global/e14fb8c9-e9e9-4069-bf85-9fa5104d9897/IMG_9666.JPG?v=1727305959289",
+    color: "hsl(20, 95%, 50%)",
+    vert: true,
+    placement: 0,
+  },
+  {
+    alttext: "passenger pocket",
+    link: "https://cdn.glitch.global/e14fb8c9-e9e9-4069-bf85-9fa5104d9897/IMG_9669.JPG?v=1727305967477",
+    color: "hsl(20, 95%, 50%)",
+    vert: false,
+    placement: 0,
+  },
+];
+
+function initMain() {
+  //about marquee
+  // let marqueetext = "";
+  // for (let i = 0; i < 40; i++) {
+  //   marqueetext +=
+  //     "<font color='#213afc'>☆</font> &emsp; about blindcam &emsp;";
+  // }
+  // marqueetext += "☆";
+  let marqueetext = "☆ ✄ ☆ ✄ ☆ ✄ ☆ ✄ ☆ ✄ ☆ ✄ ☆";
+  document.getElementById("aboutmarquee").innerHTML = marqueetext;
+
+  // main pg text
+  document.getElementById("textbox").innerHTML = "<br><br><br><br>";
+
+  //one step further.. what if i randomly rearrange the array first
+  let randomorderphotos = randomizeOrder();
+
+  // looping thru array and generating random x coordinate for placement
+  for (let i = 0; i < randomorderphotos.length; i++) {
+    let currpic = randomorderphotos[i];
+    //horizontal placement , 10 - 70
+    currpic.placement = Math.floor(Math.random() * (90 - 10 + 1)) + 5;
+
+    let newhtml =
+      "<div class='txt' style='left: " +
+      currpic.placement +
+      "%'><div class='tooltip'>" +
+      currpic.alttext;
+
+    // orient with either vertical or horizontal dimensions
+    if (currpic.vert) {
+      newhtml += "<span class='tooltiptext vert' style='border-color: ";
+    } else {
+      newhtml += "<span class='tooltiptext' style='border-color: ";
+    }
+
+    newhtml +=
+      currpic.color + "'><img src='" + currpic.link + "'/></span></div></div>";
+
+    document.getElementById("textbox").innerHTML =
+      document.getElementById("textbox").innerHTML + newhtml;
+  }
+}
+
+//helper to rearrange the photo order randomly
+function randomizeOrder() {
+  let oldphotos = photos;
+  let randomorderphotos = [];
+  let randonum = 0;
+  let chosenphoto = 0;
+
+  while (oldphotos.length > 0) {
+    let numphotos = oldphotos.length; // picking randomly from what's left
+    randonum = Math.floor(Math.random() * numphotos);
+    randomorderphotos.push(oldphotos[randonum]);
+
+    // removing from old photos array
+    if (randonum > -1) {
+      // only splice array when item is found
+      oldphotos.splice(randonum, 1); // 2nd parameter means remove one item only
+    }
+  }
+
+  return randomorderphotos;
+}
+
+function initAbout() {
+  //marquee
+  let homemarqueetext = "";
+  for (let i = 0; i < 40; i++) {
+    homemarqueetext +=
+      "<font color='#ff5500'>✂︎</font> &emsp; back 2 blindcams &emsp;";
+  }
+  homemarqueetext += "✂︎";
+  document.getElementById("homemarquee").innerHTML = homemarqueetext;
+
+  // about pg textbox
+  document.getElementById("abouttextbox").innerHTML =
+    "<br> <p>about blindcam</p>";
+}
