a5657435a3958e38fe8e8643c06c9f95c9c5fb20
ameliamylvaganam <41197477+ameliamylvaganam@users.noreply.github.com>
2026-06-05 21:56:28 -0400

    update file links to be local

--- subpages/journal/functions.js
+++ subpages/journal/functions.js
@@ -3,351 +3,349 @@ myarray = [
   {
     name: "1.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/1.mp3?v=1647629042555",
+      "library/1.mp3",
     color: "#176619",
   },
   {
     name: "2.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/2.mp3?v=1647629181997",
+      "library/2.mp3",
     color: "#cf9417",
   },
   {
     name: "3.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/3.mp3?v=1647629186970",
-    color:
-      "tch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/3.mp3?v=1647629186970",
+      "library/3.mp3",
     color: "#0b2073",
   },
   {
     name: "4.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/4.mp3?v=1647629191103",
+      "library/4.mp3",
     color: "#8f1ec7",
   },
   {
     name: "5.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/5.mp3?v=1647629195658",
+      "library/5.mp3",
     color: "#eb6426",
   },
   {
     name: "6.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/6.mp3?v=1647629200234",
+      "library/6.mp3",
     color: "#ba1c46",
   },
   {
     name: "7.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/7.mp3?v=1647629204479",
+      "library/7.mp3",
     color: "#eb2121",
   },
   {
     name: "8.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/8.mp3?v=1647629208063",
+      "library/8.mp3",
     color: "#30c2aa",
   },
   {
     name: "9.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/9.mp3?v=1647629210412",
+      "library/9.mp3",
     color: "#f01acc",
   },
   {
     name: "10.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/10.mp3?v=1647629215518",
+      "library/10.mp3",
     color: "#6eb1ff",
   },
   {
     name: "11.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/11.mp3?v=1647629217648",
+      "library/11.mp3",
     color: "#faec52",
   },
   {
     name: "12.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/12.mp3?v=1647629220248",
+      "library/12.mp3",
     color: "#fa5266",
   },
   {
     name: "13.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/13.mp3?v=1647629224165",
+      "library/13.mp3",
     color: "#6370ba",
   },
   {
     name: "14.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/14.mp3?v=1647629227274",
+      "library/14.mp3",
     color: "#ab371d",
   },
   {
     name: "15.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/15.mp3?v=1647629231282",
+      "library/15.mp3",
     color: "#c29eff",
   },
   {
     name: "16.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/16.mp3?v=1647629234071",
+      "library/16.mp3",
     color: "#d6638b",
   },
   {
     name: "17.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/17.mp3?v=1647629237824",
+      "library/17.mp3",
     color: "#ffb3f4",
   },
   {
     name: "18.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/18.mp3?v=1647629241041",
+      "library/18.mp3",
     color: "#9effca",
   },
   {
     name: "19.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/19.mp3?v=1647629246171",
+      "library/19.mp3",
     color: "#451a0b",
   },
   {
     name: "20.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/20.mp3?v=1647629248915",
+      "library/20.mp3",
     color: "#20b0ab",
   },
   {
     name: "21.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/21.mp3?v=1647629254587",
+      "library/21.mp3",
     color: "#9bde0b",
   },
   {
     name: "22.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/22.mp3?v=1647629257633",
+      "library/22.mp3",
     color: "#9e1c98",
   },
   {
     name: "23.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/23.mp3?v=1647629262260",
+      "library/23.mp3",
     color: "#e016da",
   },
   {
     name: "24.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/24.mp3?v=1647629266547",
+      "library/24.mp3",
     color: "#3d639c",
   },
   {
     name: "25.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/25.mp3?v=1647629269058",
+      "library/25.mp3",
     color: "#f7f788",
   },
   {
     name: "26.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/26.mp3?v=1647629273370",
+      "library/26.mp3",
     color: "#118a8c",
   },
   {
     name: "27.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/27.mp3?v=1647629276994",
+      "library/27.mp3",
     color: "#f0bd5d",
   },
   {
     name: "28.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/28.mp3?v=1647629281792",
+      "library/28.mp3",
     color: "#929fa1",
   },
   {
     name: "29.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/29.mp3?v=1647629285995",
+      "library/29.mp3",
     color: "#d1ffef",
   },
   {
     name: "30.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/30.mp3?v=1647629288657",
+      "library/30.mp3",
     color: "#fced3f",
   },
   {
     name: "31.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/31.mp3?v=1647629293677",
+      "library/31.mp3",
     color: "#69420d",
   },
   {
     name: "32.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/32.mp3?v=1647629298812",
+      "library/32.mp3",
     color: "#1366a1",
   },
   {
     name: "33.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/33.mp3?v=1647629301455",
+      "library/33.mp3",
     color: "#f01648",
   },
   {
     name: "34.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/34.mp3?v=1647629305844",
+      "library/34.mp3",
     color: "#a3078c",
   },
   {
     name: "35.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/35.mp3?v=1647629320267",
+      "library/35.mp3",
     color: "#5dc232",
   },
   {
     name: "36.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/36.mp3?v=1647629324110",
+      "library/36.mp3",
     color: "#8fbdb1",
   },
   {
     name: "37.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/37.mp3?v=1647629327613",
+      "library/37.mp3",
     color: "#ffd86b",
   },
   {
     name: "38.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/38.mp3?v=1647629331822",
+      "library/38.mp3",
     color: "#ff936b",
   },
   {
     name: "39.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/39.mp3?v=1647629336868",
+      "library/39.mp3",
     color: "#7480cf",
   },
   {
     name: "40.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/40.mp3?v=1647629341510",
+      "library/40.mp3",
     color: "#31c4cc",
   },
   {
     name: "41.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/41.mp3?v=1647629344886",
+      "library/41.mp3",
     color: "#210542",
   },
   {
     name: "42.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/42.mp3?v=1647629352669",
+      "library/42.mp3",
     color: "#b0b593",
   },
   {
     name: "43.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/43.mp3?v=1647629354495",
+      "library/43.mp3",
     color: "#635ec4",
   },
   {
     name: "44.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/44.mp3?v=1647629359229",
+      "library/44.mp3",
     color: "#ffa305",
   },
   {
     name: "45.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/45.mp3?v=1647629362926",
+      "library/45.mp3",
     color: "#2abf40",
   },
   {
     name: "46.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/46.mp3?v=1647629368184",
+      "library/46.mp3",
     color: "#e62e2e",
   },
   {
     name: "47.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/47.mp3?v=1647629370677",
+      "library/47.mp3",
     color: "#6637a3",
   },
   {
     name: "48.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/48.mp3?v=1647629375631",
+      "library/48.mp3",
     color: "#f08e1f",
   },
   {
     name: "49.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/49.mp3?v=1647629378388",
+      "library/49.mp3",
     color: "#ed582b",
   },
   {
     name: "water.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/water.mp3?v=1647629404086",
+      "library/water.mp3",
     color: "#8bc9bd",
   },
   {
     name: "50.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/50.mp3?v=1647629382493",
+      "library/50.mp3",
     color: "#9a36ff",
   },
   {
     name: "51.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/51.mp3?v=1647629385182",
+      "library/51.mp3",
     color: "#81c983",
   },
   {
     name: "52.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/52.mp3?v=1647629388562",
+      "library/52.mp3",
     color: "#ad0900",
   },
   {
     name: "53.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/53.mp3?v=1647629391981",
+      "library/53.mp3",
     color: "#9fa848",
   },
   {
     name: "54.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/54.mp3?v=1647629397932",
+      "library/54.mp3",
     color: "#3a694d",
   },
   {
     name: "55.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/55.mp3?v=1647629399007",
+      "library/55.mp3",
     color: "#dd7d8d",
   },
   {
     name: "56.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/56.mp3?v=1650235301217",
+      "library/56.mp3",
     color: "#036bfc",
   },
   {
     name: "57.mp3",
     audio:
-      "https://cdn.glitch.global/aaad380c-9414-4a3c-b3d2-e37f1a7054a6/57.mp3?v=1709787649901",
+      "library/57.mp3",
     color: "#0dc6ff",
   },
 ];
