29de0a0be37c277af154110c8467a07c92130404
ameliamylvaganam <41197477+ameliamylvaganam@users.noreply.github.com>
2026-06-05 23:34:50 -0400

    syntax error

--- script.js
+++ script.js
@@ -444,7 +444,7 @@ function initMain() {
   document.getElementById("homepg").innerHTML = document.getElementById("homepg").innerHTML + homepg[0].text;
   //finding link and checking if real, then autoclicking 4 homepage material
   let link = document.getElementById("homepg");
-    }
+  link.click();
   
   //about content
   document.getElementById("about").innerHTML =
