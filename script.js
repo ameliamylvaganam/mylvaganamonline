02bd1ddcfaf68c27a821d017cc3485f0ba0e6ba4
ameliamylvaganam <41197477+ameliamylvaganam@users.noreply.github.com>
2026-06-30 15:09:44 -0400

    adjust rhizome wording
    Corrected a minor grammatical issue in the text.

--- script.js
+++ script.js
@@ -380,7 +380,7 @@ let projects = [
     centeralign:
       ``,
     leftalign: [
-      `Below is a still from a clip that I have already shot, as well as a dialogue snippet, both pieces that prompted this project. I am also embedding <i>Rocks</i> and <i>Chico Baby</i>, recent works that feel tonally and structurally relevant to this proposal. Other films can be found via the PROJECTS tab.`
+      `Below is a still from a clip that I have already shot, as well as a dialogue snippet, that prompted this project. I am also embedding <i>Rocks</i> and <i>Chico Baby</i>, recent works that feel tonally and structurally relevant to this proposal. Other films can be found via the PROJECTS tab.`
     ],
     centeragain: `<br><img loading='lazy' src='subpages/assets/longdrive.png' alt="still from footage for A Long Drive Where You’re Looking Straight Ahead and I’m Looking Right At You"><br>
     <br>
