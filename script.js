ef46a04bfaf0a51a5389be7f3f58cc4df96e915e
ameliamylvaganam <41197477+ameliamylvaganam@users.noreply.github.com>
2026-06-30 15:07:40 -0400

    adding linebreaks

--- script.js
+++ script.js
@@ -382,7 +382,8 @@ let projects = [
     leftalign: [
       `Below is a still from a clip that I have already shot, as well as a dialogue snippet, both pieces that prompted this project. I am also embedding <i>Rocks</i> and <i>Chico Baby</i>, recent works that feel tonally and structurally relevant to this proposal. Other films can be found via the PROJECTS tab.`
     ],
-    centeragain: `<img loading='lazy' src='subpages/assets/longdrive.png' alt="still from footage for A Long Drive Where You’re Looking Straight Ahead and I’m Looking Right At You"><br>
+    centeragain: `<br><img loading='lazy' src='subpages/assets/longdrive.png' alt="still from footage for A Long Drive Where You’re Looking Straight Ahead and I’m Looking Right At You"><br>
+    <br>
     -Did you have any dreams?
     <br><br>
     -If I did I cant remember them
